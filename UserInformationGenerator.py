b5e45ed411d2d8a39e2a0fb3df5f3f700163ed20
meganfleck <megan.fleck@maine.edu>
2023-11-13 14:12:29 -0500

    modified:   UserInformationGenerator.py

--- UserInformationGenerator.py
+++ UserInformationGenerator.py
@@ -1,6 +1,17 @@
 #Fake User Information Generator
 import csv
+import json
 from faker import Faker #pip install Faker
-
+from volleyBallDatabase import volleyBallDatabase
+import psycopg2
 fake = Faker()
+vbDB = volleyBallDatabase()
+
+with open('config.json','r') as data:
+    config=json.load(data)
+
+connection = psycopg2.connect(**config)
+
+cursor =connection.cursor()
+
 #fake.name() <- produces randomized name
